8dc51235d4a56183d62de02242755302b7da448f
ashasoman <ashasoman13@gmail.com>
2025-09-23 11:54:51 +1000

    CI/CD config check

---
+++ frontend/src/axiosConfig.jsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+ // baseURL: 'http://localhost:5001', // local
+  baseURL: '15.134.76.220:5001', // live
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default axiosInstance;
+//demo
